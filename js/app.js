bf6ee545177884313e832e7375a17a89594d2a3c
KingIrshadh007 <kingirshadh1998@gmail.com>
2026-07-24 15:20:59 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,52 @@
+// ===============================
+// DARK MODE
+// ===============================
 
+const themeToggle = document.getElementById("themeToggle");
+
+
+if(localStorage.getItem("theme") === "dark"){
+
+    document.body.classList.add("dark-mode");
+
+    if(themeToggle){
+        themeToggle.innerHTML="☀️ Light Mode";
+    }
+
+}
+
+
+
+if(themeToggle){
+
+themeToggle.addEventListener("click",()=>{
+
+
+document.body.classList.toggle("dark-mode");
+
+
+if(document.body.classList.contains("dark-mode")){
+
+
+localStorage.setItem("theme","dark");
+
+themeToggle.innerHTML="☀️ Light Mode";
+
+
+}
+else{
+
+
+localStorage.setItem("theme","light");
+
+themeToggle.innerHTML="🌙 Dark Mode";
+
+
+}
+
+
+
+});
+
+
+}
